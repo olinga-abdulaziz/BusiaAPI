6ae4378ea5593a8b7175f3cafd741ec74b000f56
olinga-abdulaziz <abdulazizolinga17@gmail.com>
2022-06-07 15:41:19 +0300

    sduhfjsdf

--- Model/Game.js
+++ Model/Game.js
@@ -26,11 +26,11 @@ const GameModel=new mongoose.Schema({
         required:true
     },
     htr:{
-        type:String,
+        type:Number,
         required:false
     },
     atr:{
-        type:String,
+        type:Number,
         required:false
     }
 })
