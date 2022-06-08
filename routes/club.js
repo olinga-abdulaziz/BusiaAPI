681dc7798ac50a9913f435f0c7f26680076e7ba0
olinga-abdulaziz <abdulazizolinga17@gmail.com>
2022-06-08 17:37:27 +0300

    sdsd

--- routes/club.js
+++ routes/club.js
@@ -81,6 +81,7 @@ router.post('/add', async(req,res)=>{
 router.put('/game-table/:clubname',async(req,res)=>{
     try {
         const results=await Club.updateOne({clubname:req.params.id},{$set:{p:req.body.p,w:req.body.w,d:req.body.d,l:req.body.l,f:req.body.f,gd:req.body.gd,pts:req.body.pts,ga:req.body.ga}})
+            results.save()
         res.json({message:results}) 
     } catch (err) {
         console.log(err);
